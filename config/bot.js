12871506b11f5d147ac58fa685692344a9d5bf3e
Scarlaid <90851437+Scarlaid@users.noreply.github.com>
2021-09-20 15:53:55 +0700

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,22 @@
+module.exports = {
+    emojis: {
+        off: ':x:',
+        error: ':warning:',
+        queue: ':bar_chart:',
+        music: ':musical_note:',
+        success: ':white_check_mark:',
+        goodbye: ':wave:',
+        loop: ':repeat_one:',
+        loopqueue: ':repeat:',
+        notes: ':notes:',
+    },
+
+    discord: {
+        token: '', //Token here 
+        prefix: '-',
+        ownerID: '424764242965692439',
+        activity: 'with life',
+    },
+
+    filters: ['8D', 'gate', 'haas', 'phaser', 'treble', 'tremolo', 'vibrato', 'reverse', 'karaoke', 'flanger', 'mcompand', 'pulsator', 'subboost', 'bassboost', 'vaporwave', 'nightcore', 'normalizer', 'surrounding'],
+};
